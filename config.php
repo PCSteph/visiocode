c3fe3bcf3b679a86a75637b699dd2f9ace24df5c
PCSteph <steph.reina@gmail.com>
2025-10-05 12:04:18 +0200

    config

--- config.php
+++ config.php
@@ -1,8 +1,17 @@
-<?PHP
-$host="192.168.1.134";
-$base="visiocode";
-$logindb="remote";
-$passdb="Superman26$";
-$db = mysql_connect($host,$logindb,$passdb);
-mysql_select_db($base,$db);
+<?php
+$host = "192.168.1.134";
+$base = "visiocode";
+$logindb = "remote";
+$passdb = "Superman26$";
+
+// Connexion à la base de données avec mysqli
+$conn = new mysqli($host, $logindb, $passdb, $base);
+
+// Vérification de la connexion
+if ($conn->connect_error) {
+    die("Échec de la connexion : " . $conn->connect_error);
+}
+
+// Connexion réussie
+echo "Connexion réussie à la base de données.";
 ?>
