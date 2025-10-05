6ddba6b6c0f59c97100bde1a0d2a1d4384d18a41
PCSteph <steph.reina@gmail.com>
2025-10-05 12:14:54 +0200

    connect_post

--- connect_post.php
+++ connect_post.php
@@ -1,42 +1,56 @@
-<?PHP
+<?php
 session_start();
-include('./config.php');
-$req = mysql_query("SELECT id_login,login,prenom,nom,autorisation,valide,credit_classique,credit_visio FROM `client` where login='".$_POST['login_connect']."' and password='".$_POST['password_connect']."';") or die('Erreur SQL !'.mysql_error());
-$data=mysql_fetch_array($req);
-if (!$data)
-{
-  $acc="false";
-  $raison="L\'identifiant ou le mot de passe est erron�.";
+require_once('./config.php');
+
+// Connexion à la base de données
+$conn = new mysqli($host, $logindb, $passdb, $base);
+if ($conn->connect_error) {
+    die("Erreur de connexion : " . $conn->connect_error);
 }
-else
-{
-  if ($data['valide']=='0')
-  {
-  	$_SESSION['id_login']=$data['id_login'];
-        $_SESSION['login']=$data['login'];
-        $_SESSION['prenom']=$data['prenom'];
-        $_SESSION['nom']=$data['nom'];
-        $_SESSION['autorisation']=$data['autorisation'];
-        $_SESSION['page_actuel']='principal.php';
-		$_SESSION['credit_classique']=$data['credit_classique'];
-		$_SESSION['credit_visio']=$data['credit_visio'];
-		mysql_query("UPDATE client set derniere_connexion='".mktime()."' where id_login='".$data['id_login']."';") or die('Erreur SQL !'.mysql_error());
-        $acc="true";
-  }
-  else
-  {
-        $acc="false";
-        $raison="Votre compte n\'est pas activ�. En cas de probl�me, vous pouvez contacter l\'administrateur du site par email � l\'adresse suivante : visiocode@free.fr";
-  }
+
+// Préparation de la requête sécurisée
+$stmt = $conn->prepare("SELECT id_login, login, prenom, nom, autorisation, valide, credit_classique, credit_visio FROM client WHERE login = ? AND password = ?");
+$stmt->bind_param("ss", $_POST['login_connect'], $_POST['password_connect']);
+$stmt->execute();
+$result = $stmt->get_result();
+$data = $result->fetch_assoc();
+
+if (!$data) {
+    $acc = "false";
+    $raison = "L'identifiant ou le mot de passe est erroné.";
+} else {
+    if ($data['valide'] === '0') {
+        $_SESSION['id_login'] = $data['id_login'];
+        $_SESSION['login'] = $data['login'];
+        $_SESSION['prenom'] = $data['prenom'];
+        $_SESSION['nom'] = $data['nom'];
+        $_SESSION['autorisation'] = $data['autorisation'];
+        $_SESSION['page_actuel'] = 'principal.php';
+        $_SESSION['credit_classique'] = $data['credit_classique'];
+        $_SESSION['credit_visio'] = $data['credit_visio'];
+
+        // Mise à jour de la dernière connexion
+        $update = $conn->prepare("UPDATE client SET derniere_connexion = ? WHERE id_login = ?");
+        $timestamp = time();
+        $update->bind_param("ii", $timestamp, $data['id_login']);
+        $update->execute();
+
+        $acc = "true";
+    } else {
+        $acc = "false";
+        $raison = "Votre compte n'est pas activé. En cas de problème, vous pouvez contacter l'administrateur du site par email à l'adresse suivante : visiocode@free.fr";
+    }
 }
-//print_r($_POST);
-//print_r($_SESSION);
-echo "<script langage=\"javascript\">";
-if ($acc=="false") echo "alert(\"".$raison."\");";
+
+// Redirection avec message
+echo "<script>";
+if ($acc === "false") echo "alert(\"" . addslashes($raison) . "\");";
 echo "var myloc = window.location.href;";
 echo "var locarray = myloc.split(\"/\");";
-echo "window.location.href=locarray[0]+'//'+locarray[2];";
+echo "window.location.href = locarray[0] + '//' + locarray[2];";
 echo "</script>";
-//header("Location: http://sd-12936.dedibox.fr/");
-?>
 
+// Fermeture des connexions
+$stmt->close();
+$conn->close();
+?>
